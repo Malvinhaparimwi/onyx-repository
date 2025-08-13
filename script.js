b06ebb9b8b9d3f25158870ef4fb4af440e9fa809
Malvinhaparimwi <malvinhaparimwi@gmail.com>
2025-08-13 15:33:14 +0200

    New modern style initial commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+var typed = new Typed('.text', {
+  strings: ['Java Developer', 'Network Engineer', 'Robotics Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  cursorChar: '',
+  loop: true,
+})
