72c4db99594d41fba130fefe80726bb69d1e124f
Malvinhaparimwi <malvinhaparimwi@gmail.com>
2025-08-14 15:32:53 +0200

    Initial main design

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ var typed = new Typed('.text', {
   cursorChar: '',
   loop: true,
 })
+
+var typed = new Typed('.hero_text', {
+  strings: ['Java Developer', 'Network Engineer', 'Robotics Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  cursorChar: '',
+  loop: true,
+})
